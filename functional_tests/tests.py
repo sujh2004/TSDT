4f8d26482ff72c123f22e04f17211f5dbe042255
15801379269 <sujinghui@bupt.edu.cn>
2026-05-11 09:36:04 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,6 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        strt_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID,'id_list_table')
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element(By.ID,'id_list_table')
@@ -38,18 +54,17 @@ class NewVisitorTest(LiveServerTestCase):
         # 他按了回车键之后，页面更新了
         # 待办事项表格中显示了“1： Buy flowers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+
         # 页面中有显示了一个文本输入框，可以输入其他代办事项"
         # 他输入了“Send a gift to Lisi"
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 页面再次更新，他的清单上显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         # 张三想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
         self.fail( 'Finish the test! ')
