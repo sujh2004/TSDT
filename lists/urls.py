f07518352a9ce121f67c7128fe4ee7625fc2cdb0
15801379269 <sujinghui@bupt.edu.cn>
2026-05-24 14:52:38 +0800

    message 19:finish working incrementally

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,re_path
+from lists import views
+
+urlpatterns = [
+    path('new',views.new_list,name='new_list'),
+    re_path(r'^lists/(\d+)/$',views.view_list, name= 'view_list'),
+    re_path(r'^lists/(\d+)/add_item$',views.add_item, name= 'add_item'),
+]
